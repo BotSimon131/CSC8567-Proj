7bc9c284b9da6d7c6638e33056887dddc71fdf6a
BotSimon131 <hervieuxsimon@gmail.com>
2024-09-03 16:33:54 +0200

    bite2

--- django-site/CJ_Proj/CJ_Proj/settings.py
+++ django-site/CJ_Proj/CJ_Proj/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-AUTH_USER_MODEL = "myapp.MyUser"
+AUTH_USER_MODEL = "public.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
