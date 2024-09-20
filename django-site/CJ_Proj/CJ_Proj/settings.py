036e01a5f4d48f9d8a7af0958169635dd82db1a3
BotSimon131 <hervieuxsimon@gmail.com>
2024-09-20 10:38:11 +0200

    IT WORKS

--- django-site/CJ_Proj/CJ_Proj/settings.py
+++ django-site/CJ_Proj/CJ_Proj/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 LOCAL_APPS = getenv("DJANGO_APPS", "api,public").split(",")
 
 INSTALLED_APPS = [
+    'api',
+    'public',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',    
-] + LOCAL_APPS
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -77,12 +79,37 @@ WSGI_APPLICATION = 'CJ_Proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if getenv("DB_TYPE", "sqlite3") == "sqlite3":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+elif getenv("DB_TYPE") == "pgsql":
+    DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+            'NAME': getenv("DB_NAME", "django"),
+
+            'USER': getenv("DB_USER", "django"),
+
+            'PASSWORD': getenv("DB_PWSD", "django"),
+
+            'HOST': getenv("DB_HOST", "django"),
+
+            'PORT': getenv("DB_PORT", "5432"),
+
+        }
+
+    }
+
 
 
 # Password validation
--- django-site/CJ_Proj/CJ_Proj/urls.py
+++ django-site/CJ_Proj/CJ_Proj/urls.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from settings.py import LOCAL_APPS
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include("api.urls")),
+    path("", include("public.urls")),
 ]
-
-if "api" in LOCAL_APPS:
-    urlpatterns += [
-        path('api/', include("api.urls")),
-    ]
-if "public" in LOCAL_APPS:
-    urlpatterns += [
-        path("public/", include("public.urls")),
-    ]
